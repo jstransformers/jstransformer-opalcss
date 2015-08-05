a0f11379779fe6d7c90be5f454bda942da09ac2f
tunnckoCore <mameto_100@mail.bg>
2015-08-05 20:59:13 +0300

    just `.render` method

--- index.js
+++ index.js
@@ -2,17 +2,15 @@
 
 var opal = require('opalcss')
 var extend = require('extend-shallow')
-var Promise = require('promise')
 
 exports.name = 'opalcss'
-exports.inputFormats = ['opal', 'opalcss']
+exports.inputFormats = ['opal', 'opalcss', 'css', 'scss', 'sass']
 exports.outputFormat = 'css'
 
-exports.renderAsync = function (str, options, locals) {
-  return new Promise(function (resolve, reject) {
-    var opts = extend({}, options || {}, locals || {})
-    opal.process(str, opts).then(function (result) {
-      resolve(result.css)
-    }, reject)
-  })
+exports.render = function (str, opts, locals) {
+  opts = opts && typeof opts === 'object' ? opts : {}
+  locals = locals && typeof locals === 'object' ? locals : {}
+  var options = extend({}, opts, locals)
+
+  return opal.process(str, opts).css
 }
