da7104994fca03cf72d71d256f33fda0e456cfb8
tunnckoCore <mameto_100@mail.bg>
2015-08-05 22:58:05 +0300

    lint

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var opal = require('opalcss')
-var postcss = require('jstransformer-postcss')
-var extend = require('extend-shallow')
+var opal = require('opalcss');
+var postcss = require('jstransformer-postcss');
+var extend = require('extend-shallow');
 
-exports.name = 'opalcss'
-exports.inputFormats = ['opal', 'opalcss']
-exports.outputFormat = 'css'
+exports.name = 'opalcss';
+exports.inputFormats = ['opal', 'opalcss'];
+exports.outputFormat = 'css';
 
 exports.render = function (str, opts) {
-  opts = extend({plugins: [opal(opts)]}, opts)
-  return postcss.render(str, opts)
-}
+  opts = extend({plugins: [opal(opts)]}, opts);
+  return postcss.render(str, opts);
+};
